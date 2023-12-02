c0c63c20c46463dda6b328401349cf29e9566df1
Lefteris Garyfalakis <lefterisgar@gmail.com>
2023-12-02 19:50:01 +0200

    Add fishing rod

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
+let score = 0;
+let isRodDropped = false;
+let fishInterval;
+
 // Define the fish types and their point values
 const fishTypes = [
-  { type: 'salmon', points: 10, color: 'orange' },
-  { type: 'tuna', points: 20, color: 'silver' },
-  { type: 'shark', points: 50, color: 'gray' },
+  { type: 'salmon', points: 10 },
+  { type: 'tuna', points: 20 },
+  { type: 'shark', points: 50 },
 ];
-
-let score = 0;
-let timer;
-let timeRemaining = 60;
 
 // Randomly select a fish from the fishTypes array
 function getRandomFish() {
@@ -22,7 +22,7 @@ function addFish() {
 
   const fishElement = document.createElement('div');
   fishElement.className = 'fish';
-  fishElement.style.backgroundColor = fish.color;
+  fishElement.style.backgroundColor = 'blue';
   fishElement.dataset.type = fish.type;
   fishElement.dataset.points = fish.points;
 
@@ -37,32 +37,6 @@ function addFish() {
 
   fishElement.style.left = randomX + 'px';
   fishElement.style.top = randomY + 'px';
-
-  // Remove the fish after a certain time
-  setTimeout(() => {
-    fishContainer.removeChild(fishElement);
-  }, 2000);
-}
-
-// Update the score
-function updateScore(points) {
-  score += points;
-  const scoreElement = document.getElementById('score');
-  scoreElement.textContent = `Score: ${score}`;
-}
-
-// Start the timer
-function startTimer() {
-  const timerElement = document.getElementById('timer');
-
-  timer = setInterval(() => {
-    timeRemaining--;
-    timerElement.textContent = `Time: ${timeRemaining}s`;
-
-    if (timeRemaining === 0) {
-      endGame();
-    }
-  }, 1000);
 }
 
 // Handle the fish click event
@@ -75,55 +49,53 @@ function fishClicked(event) {
   updateScore(fishPoints);
 }
 
+// Update the score
+function updateScore(points) {
+  score += points;
+  const scoreElement = document.getElementById('score');
+  scoreElement.textContent = `Score: ${score}`;
+}
+
 // Handle the space key press event
 function handleKeyPress(event) {
   if (event.keyCode === 32) { // Space key
-    const fishContainer = document.getElementById('fishContainer');
-    const fishElements = fishContainer.getElementsByClassName('fish');
+    if (!isRodDropped) {
+      isRodDropped = true;
+      fishInterval = setInterval(addFish, 1000);
+    }
+    else {
+      const fishContainer = document.getElementById('fishContainer');
+      const fishElements = fishContainer.getElementsByClassName('fish');
 
-    for (let i = 0; i < fishElements.length; i++) {
-      const fishElement = fishElements[i];
+      if (fishElements.length > 0) {
+        const nearestFish = fishElements[0];
 
-      if (fishElement.getBoundingClientRect().top < 100) {
-        const fishType = fishElement.dataset.type;
-        const fishPoints = parseInt(fishElement.dataset.points);
+        const rodPosition = 250; // Adjust this value based on the rod position
+        const fishPosition = nearestFish.getBoundingClientRect().left;
 
-        fishElement.parentNode.removeChild(fishElement);
-        updateScore(fishPoints);
+        if (fishPosition >= rodPosition && fishPosition <= rodPosition + 100) {
+          const fishType = nearestFish.dataset.type;
+          const fishPoints = parseInt(nearestFish.dataset.points);
+
+          nearestFish.parentNode.removeChild(nearestFish);
+          updateScore(fishPoints);
+        }
       }
     }
   }
 }
 
-// End the game
-function endGame() {
-  clearInterval(timer);
-  document.removeEventListener('keypress', handleKeyPress);
-  alert(`Game Over!\nScore: ${score}`);
-}
-
 // Start the fishing game
 function startGame() {
   score = 0;
-  timeRemaining = 60;
+  isRodDropped = false;
+  clearInterval(fishInterval);
+
   const fishContainer = document.getElementById('fishContainer');
   fishContainer.innerHTML = '';
 
   const scoreElement = document.getElementById('score');
   scoreElement.textContent = 'Score: 0';
 
-  const timerElement = document.getElementById('timer');
-  timerElement.textContent = `Time: ${timeRemaining}s`;
-
-  // Add fish every 1 second
-  setInterval(addFish, 1000);
-
-  // Add click event listener to each fish
-  fishContainer.addEventListener('click', fishClicked);
-
-  // Add keypress event listener for the space bar
   document.addEventListener('keypress', handleKeyPress);
-
-  // Start the timer
-  startTimer();
 }
